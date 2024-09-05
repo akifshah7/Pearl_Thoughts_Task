721a7484275e57b18c6d5b2498c8277ce582a677
Akif <akifshah7@gmail.com>
2024-09-05 13:48:56 +0530

    init

---
+++ src/components/date-picker/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DatePicker: React.FC = () => {
+  return (
+    <input
+      type="date"
+      className="h-9 w-64 px-2 border-1 border border-gray-300 rounded-md"
+    />
+  );
+};
+
+export default DatePicker;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DatePicker from '~/components/date-picker'
+import '~/global.css'
+
+const App = () => {
+  return (
+    <div className='w-full min-h-screen flex justify-center p-4'>
+        <DatePicker />  
+    </div>
+  )
+}
+
+export default App
