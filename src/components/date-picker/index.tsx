dfc384f7105b5711563c9ca0d715f87297e48d30
Akif <akifshah7@gmail.com>
2024-09-07 14:05:02 +0530

    removed unused packages

--- src/components/date-picker/index.tsx
+++ src/components/date-picker/index.tsx
@@ -1,35 +1,30 @@
 import React from "react";
-import { DateRangePicker } from "@nextui-org/date-picker";
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import useDatePickerStore from "~/store/datePicker.store";
 
 const DatePickerComponent = () => {
   const { value, setValue } = useDatePickerStore();
 
+  const handleChange = (dates) => {
+    const [start, end] = dates;
+    setValue({
+      start: start,
+      end: end,
+    });
+  };
+
   return (
-    <DateRangePicker
-      label="Select Date"
-      value={value}
-      onChange={setValue}
-      calendarProps={{
-        classNames: {
-          base: "bg-white",
-          headerWrapper: "p-4 bg-background",
-          prevButton: "border-1 border-default-200 rounded-small",
-          nextButton: "border-1 border-default-200 rounded-small",
-          gridHeader: "bg-background shadow-none border-b-1 border-default-100",
-          cellButton: [
-            "data-[today=true]:bg-default-100 data-[selected=true]:bg-transparent rounded-small",
-            "data-[range-start=true]:before:rounded-l-small",
-            "data-[selection-start=true]:before:rounded-l-small",
-            "data-[range-end=true]:before:rounded-r-small",
-            "data-[selection-end=true]:before:rounded-r-small",
-            "data-[selected=true]:data-[selection-start=true]:data-[range-selection=true]:rounded-small",
-            "data-[selected=true]:data-[selection-end=true]:data-[range-selection=true]:rounded-small",
-          ],
-        },
-      }}
-      className="w-96 border-2 h-16 bg-gray-100 rounded-md border-gray-400 px-2 py-1"
-    />
+    <div className="w-full flex justify-center" >
+      <ReactDatePicker
+   
+        selectsRange
+        dateFormat="yyyy-MM-dd"
+        className="w-96 flex justify-center items-center bg-gray-100 border-2 border-gray-400 rounded-md p-2"
+        placeholderText="Select a date range"
+        isClearable
+      />
+      </div>
   );
 };
 
